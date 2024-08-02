84d370db453143b51f4ebb2111f736384ba03c9d
Zzihua <C109156129@nkust.edu.tw>
2024-08-02 15:14:48 +0800

    import asset

--- workshop/resources/views/layout/master.blade.php
+++ workshop/resources/views/layout/master.blade.php
@@ -1,10 +1,15 @@
 <html> 
     <head> 
         <meta charset="utf-8"> <title>@yield('title') - Shop Laravel</title> 
-        <script src="/assets/js/jquery-2.2.4.min.js" defer></script> 
-        <script src="/assets/js/bootstrap.min.js" defer></script> 
-        <link rel="stylesheet" href="/assets/css/bootstrap.min.css"> 
-        <link rel="stylesheet" href="/assets/css/font-awesome.min.css">     
+        <script src="{{asset('assets/js/jquery-2.2.4.min.js')}}" defer></script> 
+        <script src="{{asset('assets/js/bootstrap.min.js')}}" defer></script> 
+        <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}"> 
+        <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}"> 
+        
+        <!-- <script src="/assets/js/jquery-2.2.4.min.js" defer></script>  -->
+        <!-- <script src="/assets/js/bootstrap.min.js" defer></script> 
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">  -->
+        <!-- <link rel="stylesheet" href="/assets/css/font-awesome.min.css">      -->
     </head> 
     <body> 
         <header> 
