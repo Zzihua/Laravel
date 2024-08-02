cb7db620e36a24f91c9d6ad63823f400bcd1da3c
Zzihua <C109156129@nkust.edu.tw>
2024-08-02 14:29:17 +0800

    section content

--- workshop/resources/views/auth/signup.blade.php
+++ workshop/resources/views/auth/signup.blade.php
@@ -1,3 +1,12 @@
+<!-- 指定繼承 layout.master 母模板 --> 
+@extends('layout.master') 
+
+<!-- 傳送資料到母模板，並指定變數為 title --> 
+@section('title', $title) 
+
+<!-- 傳送資料到母模板，並指定變數為 content -->
+@section('content') 
+
 <h1>{{ $title }}</h1> 
 <h3>{{ $subtitle }}</h3> 
 <div class="social"> 
@@ -6,4 +15,6 @@
 </div> 
 Email： <input type="text" name="email" placeholder="Email" > 
 密碼： <input type="password" name="password" placeholder="密碼" > 
-暱稱： <input type="text" name="nickname" placeholder="暱稱" > 
+暱稱： <input type="text" name="nickname" placeholder="暱稱" >
+
+@endsection  
